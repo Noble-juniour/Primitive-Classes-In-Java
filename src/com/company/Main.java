b701497b8ce96523334c571e79636f9f95f71648
kevin <kevinindangasi1@gmail.com>
2022-03-22 16:34:16 +0300

    Primitive Classes

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String s = "String";
+    char c = 'c';
+
+    //Character c = 'c';
+        System.out.println(Character.isLowerCase(c));
+
+    }
+}
